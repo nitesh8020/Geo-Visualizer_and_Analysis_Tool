371b7ceeecdb2ae890790c0815479cf9b0e2322d
nitesh <kumarnitesh8020@gmail.com>
2020-11-23 23:10:23 +0530

    Added code for classification

--- classification.py
+++ classification.py
@@ -2,8 +2,15 @@ import tkinter as tk
 from tkinter import ttk
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 from tkinter import filedialog
+import statistics as st
+import pandas as pd
+from sklearn.cluster import KMeans
+from skimage.io import imread
+import earthpy.plot as ep
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -14,6 +21,33 @@ class segmentation(ttk.Frame):
         self.master = master
         self.display=ttk.Frame(self)
         self.createWidgets()
+        self.files=[]
+
+    def create(self):
+        if self.display.winfo_exists():
+            self.display.grid_forget()
+            self.display.destroy()
+            
+        self.display = ttk.Frame(self)
+        self.display.grid(row = 0, column = 4,rowspan=2, sticky = 'nwes')
+        frame=self.display
+        
+        file=self.files[0]
+        image = imread('band.tiff') / 255
+        l,b,c = image.shape
+
+        rescale_image = image.reshape(l*b,c)
+        kmeans = KMeans(n_clusters=2, random_state=0).fit(rescale_image)
+        clustered = kmeans.cluster_centers_[kmeans.labels_]
+        final_image = clustered.reshape(image.shape[0], image.shape[1], 3)
+        self.figure = Figure(figsize = (6,4), dpi = 100)
+        self.plot = self.figure.add_subplot(1,1,1)
+        self.plot.imshow(final_image)
+        self.canvas = FigureCanvasTkAgg(self.figure,frame)
+        self.toolbar = NavigationToolbar2Tk(self.canvas,frame)
+        self.toolbar.update()
+        self.canvas.get_tk_widget().pack()
+
 
     def createWidgets(self):
         self.grid_columnconfigure(0,weight = 0)
@@ -24,3 +58,26 @@ class segmentation(ttk.Frame):
 
         self.panel = ttk.Frame(self)
         self.panel.grid(row=1, column=0, sticky='nsew')
+
+        self.down = ttk.Frame(self)
+        self.down.grid(row = 2, column = 0, sticky = 'sw', columnspan = 4)
+        
+        self.txt = ttk.Label(self.panel, text="K-Means Segmentation", font="Arial 15 bold")
+        self.txt.grid(row=0, column=0,sticky='wens', padx=10, pady=10)
+        
+        self.btn = ttk.Button(self.panel, text='Choose Image', command=self.choose)
+        self.btn.grid(row=1, column=0, sticky='nsew', padx=10, pady=10)
+
+        self.createbtn = tk.Button(self.down, text='CREATE SEGMENTATION',bg='green', fg='white', command=self.create, state='disabled')
+        self.createbtn.grid(row=4, column=0, sticky='nsew', padx=10, pady=10, columnspan=1)
+
+
+
+    def choose(self, event=None):            # event handler for choose file
+        ffile = filedialog.askopenfilename()
+        if(ffile!=() and ffile!=''):
+            ind= ffile.rfind('/')
+            btn = ttk.Button(self.panel, text = ffile[ind+1:])
+            btn.grid(row=4,column=0, sticky='nsew', padx=10, pady=10 )
+            self.files.append(ffile)
+            self.createbtn['state']='normal'
