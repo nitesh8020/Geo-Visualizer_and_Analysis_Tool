cd55c4ec9b4d861e9fe3dcf40d7839a84b59f9c8
ShikhaSuman8601 <shikhadarshi@gmail.com>
2020-11-24 16:52:21 +0530

    Commenting and optimizing

--- interface.py
+++ interface.py
@@ -1,3 +1,7 @@
+'''Main file to implement all the functions and creating the main GUI
+To run the main program, run this file'''
+
+
 from tkinter import ttk
 from ttkthemes import ThemedStyle
 from glob import glob
@@ -18,8 +22,7 @@ import os
 from Indices import Index
 from datetime import datetime
 from classification import segmentation
-#clip file
-# from clipmosaic import clipping
+from clipmosaic import clipping
 
 
 class RootFrame(ttk.Frame):
@@ -34,7 +37,7 @@ class RootFrame(ttk.Frame):
         self.window=tk.Frame(self)
         self.createWidgets()            # call to create widgets
         
-    def createWidgets(self):
+    def createWidgets(self):            # create widgets
 
         self.grid_columnconfigure(0,weight=0)
         self.grid_columnconfigure(1,weight=0)
@@ -48,22 +51,21 @@ class RootFrame(ttk.Frame):
         
         self.panel=ttk.Frame(self)
         self.panel.grid(row=0, column=0, sticky='nsew')
-        
-        self.menuBtn1 = ttk.Button(self.panel, text="Input", command=self.getInput)
+
+        # Buttons to navigate between frames
+        self.menuBtn1 = ttk.Button(self.panel, text="Feed Data", command=self.getInput)
         self.menuBtn1.grid(row=0, column=0)
         
-        self.menuBtn2 = ttk.Button(self.panel, text="Process", command=self.Process)
+        self.menuBtn2 = ttk.Button(self.panel, text="Analysis", command=self.Process)
         self.menuBtn2.grid(row=0, column=1)
         
-        self.menuBtn3 = ttk.Button(self.panel, text="Index", command=self.calIndex)
+        self.menuBtn3 = ttk.Button(self.panel, text="Compute Indices", command=self.calIndex)
         self.menuBtn3.grid(row=0, column=2)
 
-        #$
         self.menuBtn4 = ttk.Button(self.panel, text="Clip Mosaic", command=self.clipping)
         self.menuBtn4.grid(row=0, column=3)
-        #$
 
-        self.menuBtn5 = ttk.Button(self.panel, text="Classification", command=self.segmentation)
+        self.menuBtn5 = ttk.Button(self.panel, text="Classify Data", command=self.segmentation)
         self.menuBtn5.grid(row=0, column=4)
         
         self.header = ttk.Label(self, text="",font="Arial 15 bold")
@@ -72,33 +74,34 @@ class RootFrame(ttk.Frame):
         self.getInput()
 
                 
-    def getInput(self, event=None):
+    def getInput(self, event=None):             # Function for Getting input and displaying image using ViewImage.py'
         if self.window.winfo_exists():
             self.window.grid_forget()
             self.window.destroy()
-        self.header['text'] = 'Input'
+        self.header['text'] = 'Feed Input Data Here'
         self.window = ViewImage(self)
         self.window.grid(row=1, column=0, columnspan=4, sticky='nsew')
         
         
-    def Process(self, event=None):
+    def Process(self, event=None):              # Function to implement Process.py for analysing and process.py
         if self.window.winfo_exists():
             self.window.grid_forget()
             self.window.destroy()
-        self.header['text'] = 'Process'
+        self.header['text'] = 'Analysis and Processing of data'
         self.window = Process(self)
         self.window.grid(row=1, column=0, columnspan=4, sticky='nsew')
-    #$
-    def clipping(self, event=None):
+    
+
+    def clipping(self, event=None):             # Function to Clip the mosaic using 'clipping.py'
         if self.window.winfo_exists():
             self.window.grid_forget()
             self.window.destroy()
-        self.header['text'] = 'Clipping'
+        self.header['text'] = 'Clip the Mosaic Image'
         self.window = clipping(self)
         self.window.grid(row=1, column=0, columnspan=4, sticky='nsew')
-    #$
     
-    def segmentation(self, event=None):
+    
+    def segmentation(self, event=None):         #Function to classify processed image image segmentation
         if self.window.winfo_exists():
             self.window.grid_forget()
             self.window.destroy()
@@ -107,11 +110,11 @@ class RootFrame(ttk.Frame):
         self.window.grid(row=1, column=0, columnspan=4, sticky='nsew')
 
     
-    def calIndex(self, event=None):
+    def calIndex(self, event=None):             #   Function to compute indices using 'Index.py'
         if self.window.winfo_exists():
             self.window.grid_forget()
             self.window.destroy()
-        self.header['text'] = 'Indices'
+        self.header['text'] = 'Compute Indices'
         self.window = Index(self)
         self.window.grid(row=1, column=0, columnspan=4, sticky='nsew')
 
